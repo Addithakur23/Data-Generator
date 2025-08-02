f6b447e4f2c422e35a2f237dc28befbcd9e17752
Addithakur23 <165467141+Addithakur23@users.noreply.github.com>
2025-08-02 23:29:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ import cors from "cors"
 
 const app = express()
 app.use(express.json())
-app.use(cors({origin:"*"}))
+app.use(cors({origin:"*",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']})) 
 const port = 3000
 const dummyDataSchema=new mongoose.Schema({
     name:String,
@@ -19,24 +21,34 @@ const dummyDataSchema=new mongoose.Schema({
 const Dummydata=mongoose.model("company",dummyDataSchema,"employees")
 
 let conn = await mongoose.connect("mongodb://localhost:27017/company")
-let name = ["Harry", "Jatin", "Aditya"]
-let city = ["New York", "London", "Dubai"]
-let language = ["English", "Hindi", "Arabic"]
+let name = ["Harry", "Jatin", "Aditya","Rohan"]
+let city = ["New York", "London", "Dubai","Russia"]
+let language = ["Python", "C++", "Javascript","Java"]
 
+const getrandom=(arr)=>{
+   let  randNo=Math.floor(Math.random()*(arr.length-1))
+    return arr[randNo]
+}
 
-app.get('/g', async (req, res) => {  
+app.post('/g', async (req, res) => {  
     // console.log("Reached post request")
     // random values generator
-    let random_boolean = Math.floor(Math.random()*2)
-    let random_salary = Math.floor((Math.random() * 30000 + Math.random() * 100000))  
-    let random = Math.floor((3 * Math.random()))
-
+    
     await Dummydata.deleteMany({})
+    let dummyData 
     // inserting data into company
-    const dummyData = new Dummydata({ name: name[random], salary: random_salary, city: city[random], language: language[random], isManager:Boolean(random_boolean)}) 
-    await dummyData.save() 
-    await Dummydata.find({})
-    res.json(dummyData)
+    for (let index = 0; index < 10; index++) {
+        let random_boolean = Math.floor(Math.random()*2)
+        let random_salary = Math.floor((Math.random() * 30000 + Math.random() * 100000))  
+        dummyData = new Dummydata({ name: getrandom(name), salary: random_salary, city: getrandom(city), language: getrandom(language), isManager:Boolean(random_boolean)}) 
+        await dummyData.save() 
+        
+    }
+    let data=await Dummydata.find({})
+    
+       
+    res.json(data)
+ 
  
 }) 
 

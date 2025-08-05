30755ea758665b8b618dda45981098f3c2c3efcd
Addithakur23 <165467141+Addithakur23@users.noreply.github.com>
2025-08-05 21:30:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,55 +1,52 @@
 import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
-
+import {Dummydata} from "./models/company.js"
 
 const app = express()
 app.use(express.json())
-app.use(cors({origin:"*"})) 
+app.use(cors({
+    origin: "*"
+}))
 const port = 3000
-const dummyDataSchema=new mongoose.Schema({
-    name:String,
-    salary: Number,
-    language: String,
-    city: String,
-    isManager: Boolean
 
-})
-
-const Dummydata=mongoose.model("company",dummyDataSchema,"employees")
 
 let conn = await mongoose.connect("mongodb://localhost:27017/company")
-let name = ["Harry", "Jatin", "Aditya","Rohan"]
-let city = ["New York", "London", "Dubai","Russia"]
-let language = ["Python", "C++", "Javascript","Java"]
+let name = ["Harry", "Jatin", "Aditya", "Rohan"]
+let city = ["New York", "London", "Dubai", "Russia"]
+let language = ["Python", "C++", "Javascript", "Java"]
 
 // random values generator
-const getrandom=(arr)=>{
-   let  randNo=Math.floor(Math.random()*(arr.length-1))
+const getrandom = (arr) => {
+    let randNo = Math.floor(Math.random() * (arr.length - 1))
     return arr[randNo]
 }
 
-app.post('/g', async (req, res) => {
-    
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
+
+app.get('/g', async (req, res) => {
+
     await Dummydata.deleteMany({})
-    let dummyData 
+    let dummyData
     // inserting data into company
     for (let index = 0; index < 10; index++) {
         // random values generator
-        let random_boolean = Math.floor(Math.random()*2)
-        let random_salary = Math.floor((Math.random() * 30000 + Math.random() * 100000))  
-        dummyData = new Dummydata({ name: getrandom(name), salary: random_salary, city: getrandom(city), language: getrandom(language), isManager:Boolean(random_boolean)}) 
-        await dummyData.save() 
-        
+        let random_boolean = Math.floor(Math.random() * 2)
+        let random_salary = Math.floor((Math.random() * 30000 + Math.random() * 100000))
+        dummyData =  new Dummydata({ name: getrandom(name), salary: random_salary, city: getrandom(city), language: getrandom(language), isManager: Boolean(random_boolean) })
+        await dummyData.save()
+
     }
-    let data=await Dummydata.find({})  
+    let data = await Dummydata.find({})
     res.json(data)
 })
 
 app.listen(port, () => {
+    console.log("Hello")
     console.log(`Example app listening on port ${port}`)
 })
 
 
 
- 
